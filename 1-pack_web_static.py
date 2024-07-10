5686096c0a2e5f02473347ebe69444951ad93fd9
Stevecmd <murimi101@gmail.com>
2024-07-10 09:37:59 +0300

    1-pack_web_static.py

---
+++ 1-pack_web_static.py
@@ -0,0 +1,31 @@
+#!/usr/bin/python3
+"""
+Fabric script to generate a .tgz archive from web_static folder
+"""
+
+import os
+from fabric.api import local
+from datetime import datetime
+
+
+def do_pack():
+    """
+    Generates a .tgz archive from web_static folder
+
+    Returns:
+        str: Path to the generated archive if successful, None otherwise
+    """
+    if not os.path.exists("versions"):
+        os.makedirs("versions")
+
+    now = datetime.now()
+    timestamp = now.strftime("%Y%m%d%H%M%S")
+    archive_path = "versions/web_static_{}.tgz".format(timestamp)
+
+    try:
+        local("tar -cvzf {} web_static".format(archive_path))
+        print("web_static packed: {} -> {} Bytes".format(archive_path, os.path.getsize(archive_path)))
+        return archive_path
+    except Exception as e:
+        print("Error packing web_static:", str(e))
+        return None
