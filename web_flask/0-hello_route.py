00d43e73f91d9749642c3bced43d9466a06827dc
Stevecmd <murimi101@gmail.com>
2024-07-27 01:59:01 +0300

    reduced top comment

--- web_flask/0-hello_route.py
+++ web_flask/0-hello_route.py
@@ -1,7 +1,5 @@
 #!/usr/bin/python3
-"""
-A simple Flask web application that listens on 0.0.0.0, port 5000
-and displays "Hello HBNB!" at the root URL.
+"""Flask web application that listens on 0.0.0.0, port 5000
 """
 from flask import Flask
 
@@ -10,9 +8,11 @@ app = Flask(__name__)
 
 @app.route('/', strict_slashes=False)
 def home():
-    """Returns a string 'Hello HBNB!'"""
+    """
+    Returns a string 'Hello HBNB!'.
+    """
     return 'Hello HBNB!'
 
 
-if __name__ == "__main__":
-    app.run(host="0.0.0.0", port=5000)
+if __name__ == '__main__':
+    app.run(host='0.0.0.0', port=5000)
