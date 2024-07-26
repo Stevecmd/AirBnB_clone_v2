612f115565af12ac06e5b6f2dd984395ba95e918
Stevecmd <murimi101@gmail.com>
2024-07-27 00:40:32 +0300

    5-number_template.py

--- web_flask/5-number_template.py
+++ web_flask/5-number_template.py
@@ -1,1 +1,49 @@
 #!/usr/bin/python3
+"""
+A simple Flask web application that listens on 0.0.0.0, port 5000
+and displays:
+- "Hello HBNB!" at the root URL
+- "HBNB" at /hbnb
+- "C " followed by the value of the text variable at /c/<text>
+- "Python " followed by the value of the text variable at /python/<text> (default is "is cool")
+- "n is a number" only if n is an integer at /number/<n>
+- A HTML page with "Number: n" inside an H1 tag at /number_template/<n> only if n is an integer
+"""
+
+from flask import Flask, render_template
+
+app = Flask(__name__)
+
+@app.route('/', strict_slashes=False)
+def hello_hbnb():
+    """Returns a string 'Hello HBNB!'"""
+    return "Hello HBNB!"
+
+@app.route('/hbnb', strict_slashes=False)
+def hbnb():
+    """Returns a string 'HBNB'"""
+    return "HBNB"
+
+@app.route('/c/<text>', strict_slashes=False)
+def c_text(text):
+    """Returns 'C ' followed by the value of the text variable"""
+    return "C " + text.replace('_', ' ')
+
+@app.route('/python/', defaults={'text': 'is cool'}, strict_slashes=False)
+@app.route('/python/<text>', strict_slashes=False)
+def python_text(text):
+    """Returns 'Python ' followed by the value of the text variable"""
+    return "Python " + text.replace('_', ' ')
+
+@app.route('/number/<int:n>', strict_slashes=False)
+def number(n):
+    """Returns 'n is a number' only if n is an integer"""
+    return f"{n} is a number"
+
+@app.route('/number_template/<int:n>', strict_slashes=False)
+def number_template(n):
+    """Renders an HTML page with 'Number: n' inside an H1 tag only if n is an integer"""
+    return render_template('number.html', n=n)
+
+if __name__ == "__main__":
+    app.run(host="0.0.0.0", port=5000)
