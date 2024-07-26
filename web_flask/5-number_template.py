761bc5cf0464614f1ccba77d0397c13470f9bb13
Stevecmd <murimi101@gmail.com>
2024-07-27 02:06:45 +0300

    test

--- web_flask/5-number_template.py
+++ web_flask/5-number_template.py
@@ -49,7 +49,7 @@ def number(n):
 @app.route('/number_template/<int:n>', strict_slashes=False)
 def number_template(n):
     """Renders an HTML page with 'Number: n'"""
-    return render_template('number.html', n=n)
+    return render_template('5-number.html', n=n)
 
 
 if __name__ == "__main__":
