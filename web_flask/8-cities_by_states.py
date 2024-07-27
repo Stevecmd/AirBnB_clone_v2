dc18d30f0d08f5b1ab0a3423f33b19686a3cda43
Stevecmd <murimi101@gmail.com>
2024-07-27 19:57:49 +0300

    8-cities_by_states.py

--- web_flask/8-cities_by_states.py
+++ web_flask/8-cities_by_states.py
@@ -1,1 +1,29 @@
 #!/usr/bin/python3
+"""
+Flask application that lists all states and cities from DBStorage
+"""
+
+from flask import Flask, render_template
+from models import storage
+from models.state import State
+
+app = Flask(__name__)
+
+
+@app.teardown_appcontext
+def teardown_db(exception):
+    """Closes the storage on teardown"""
+    storage.close()
+
+
+@app.route('/cities_by_states', strict_slashes=False)
+def cities_by_states():
+    """Displays a HTML page with a list of all State objects
+        and their linked City objects
+    """
+    states = storage.all(State)
+    return render_template('8-cities_by_states.html', states=states)
+
+
+if __name__ == "__main__":
+    app.run(host='0.0.0.0', port=5000)
