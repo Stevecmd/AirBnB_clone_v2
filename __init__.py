77c02cd3e51a1f27e0347f0ac831817980e502e8
Stevecmd <murimi101@gmail.com>
2024-07-27 10:39:57 +0300

    added comments to init files

--- __init__.py
+++ __init__.py
@@ -1,1 +1,6 @@
 #!/usr/bin/python3
+"""
+This module initializes the package and sets up
+the necessary imports. It ensures that the package
+components are accessible when the package is imported.
+"""
