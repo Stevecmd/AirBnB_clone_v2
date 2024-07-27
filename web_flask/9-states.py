4b519cede90a0440d59f9c65265119d5ee28ca46
Stevecmd <murimi101@gmail.com>
2024-07-27 20:11:07 +0300

    9-states.py

--- web_flask/9-states.py
+++ web_flask/9-states.py
@@ -1,1 +1,36 @@
 #!/usr/bin/python3
+"""
+Flask application that lists all states and cities from DBStorage
+"""
+
+from flask import Flask, render_template
+from models import storage
+
+app = Flask(__name__)
+
+
+@app.teardown_appcontext
+def teardown_db(exc):
+    """Closes the storage on teardown."""
+    storage.close()
+
+
+@app.route("/states", strict_slashes=False)
+def states():
+    """Displays a HTML page with a list of all State objects
+    """
+    states = storage.all("State")
+    return render_template("9-states.html", state=states)
+
+
+@app.route("/states/<id>", strict_slashes=False)
+def states_id(id):
+    """Displays a HTML page with a list of City objects linked to the State."""
+    for state in storage.all("State").values():
+        if state.id == id:
+            return render_template("9-states.html", state=state)
+    return render_template("9-states.html")
+
+
+if __name__ == "__main__":
+    app.run(host="0.0.0.0")
