14ecf2dfa9722a0fe54b0c3648a5271540b26ab1
Stevecmd <murimi101@gmail.com>
2024-07-28 12:40:50 +0300

    pycodestyle updates

--- models/state.py
+++ models/state.py
@@ -14,8 +14,16 @@ class State(BaseModel, Base):
     __tablename__ = 'states'
 
     name = Column(String(128), nullable=False)
-    cities = relationship("City", back_populates="state", cascade="all, delete-orphan")
-    places = relationship("Place", back_populates="state", cascade="all, delete-orphan")
+    cities = relationship(
+        "City",
+        back_populates="state",
+        cascade="all, delete-orphan"
+    )
+    places = relationship(
+        "Place",
+        back_populates="state",
+        cascade="all, delete-orphan"
+    )
 
     def __init__(self, *args, **kwargs):
         """Initializes state"""
