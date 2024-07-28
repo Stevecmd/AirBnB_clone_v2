0994863e294a04c65a213ed02d113dce2d105d65
Stevecmd <murimi101@gmail.com>
2024-07-28 10:52:32 +0300

    removed redeclaration of base class

--- models/user.py
+++ models/user.py
@@ -6,7 +6,6 @@ from models.base_model import BaseModel, Base
 from os import getenv
 from sqlalchemy import Column, String
 from sqlalchemy.orm import relationship
-from sqlalchemy.ext.declarative import declarative_base
 
 
 """
@@ -14,14 +13,13 @@ This module defines a class User that inherits from BaseModel and Base
 """
 
 
-Base = declarative_base()
 storage_type = getenv('HBNB_TYPE_STORAGE')
 
 
 class User(BaseModel, Base):
     """Representation of a user """
+    __tablename__ = 'users'
     if storage_type == 'db':
-        __tablename__ = 'users'
         email = Column(String(128), nullable=False)
         password = Column(String(128), nullable=False)
         first_name = Column(String(128), nullable=True)
