61fa6f9b72c004f4e1acecc1fd7ef7e6f4380bf1
Stevecmd <murimi101@gmail.com>
2024-07-30 01:48:43 +0300

    9-states.py

--- web_flask/9-states.py
+++ web_flask/9-states.py
@@ -20,16 +20,16 @@ def states():
     """Displays a HTML page with a list of all State objects
     """
     states = storage.all("State")
-    return render_template("9-states.html", state=states)
+    return render_template("9-states.html", states=states)
 
 
 @app.route("/states/<id>", strict_slashes=False)
 def states_id(id):
     """Displays a HTML page with a list of City objects linked to the State."""
-    for state in storage.all("State").values():
-        if state.id == id:
-            return render_template("9-states.html", state=state)
-    return render_template("9-states.html")
+    state = storage.all("State").get(id)
+    if state:
+        return render_template("9-states.html", state=state)
+    return render_template("9-states.html", state=None)
 
 
 if __name__ == "__main__":
