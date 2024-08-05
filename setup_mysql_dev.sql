117670d5c82191285aad593d67b784ba6f56526f
SteveCmd <murimi101@gmail.com>
2024-08-05 19:29:13 +0300

    updated mysql dev setup to include states table

--- setup_mysql_dev.sql
+++ setup_mysql_dev.sql
@@ -1,4 +1,3 @@
--- Create database if it doesn't exist
 CREATE DATABASE IF NOT EXISTS hbnb_dev_db;
 
 -- Create user if it doesn't exist and set password
@@ -12,3 +11,14 @@ GRANT ALL PRIVILEGES ON hbnb_dev_db.* TO 'hbnb_dev'@'localhost';
 -- Grant SELECT privilege on performance_schema to hbnb_dev
 GRANT SELECT ON performance_schema.* TO 'hbnb_dev'@'localhost';
 FLUSH PRIVILEGES;
+
+-- Use the hbnb_dev_db database
+USE hbnb_dev_db;
+
+-- Create the states table
+CREATE TABLE IF NOT EXISTS states (
+    id CHAR(36) NOT NULL PRIMARY KEY,
+    created_at DATETIME NOT NULL,
+    updated_at DATETIME NOT NULL,
+    name VARCHAR(128) NOT NULL
+);
